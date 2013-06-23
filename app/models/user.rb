3541184201661a34fde2db1830da7c71fd479767
Ken Lu <ken@seeice.com>
2013-06-23 09:55:32 -0400

    devise controller and mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
+  attr_accessible :email, :password
 end
