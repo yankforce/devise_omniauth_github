b5ed833bfcb23cce6c520cc99145bc7ef5198731
Ken Lu <ken@seeice.com>
2013-06-23 15:48:53 -0400

    model change for create user after authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,29 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
   # attr_accessible :title, :body
-  attr_accessible :email, :password
+  attr_accessible :email, :password, :password_confirmation, :provider, :uid, :name
 
-  def self.from_omniauth(auth)
-    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
-  end
-
-  def self.create_from_omniauth(auth)
-    user = User.new
-    user.provider = auth["provider"]
-    user.uid = auth["uid"]
-    user.name = auth["info"]["nickname"]
-    user.email = auth['info']['email']
-    user.save(validate: false)
+  def self.find_for_github_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+                        )
+    end
     user
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
 end
