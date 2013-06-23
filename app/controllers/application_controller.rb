d50b6873cbf549bf257eacee262c2f9578b170d8
Ken Lu <ken@seeice.com>
2013-06-23 15:48:18 -0400

    sign in hook and sign out hook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def after_sign_in_path_for(resource)
-    current_user_path
+    #current_user
+    root_path
   end
   #def after_sign_out_path_for(resource)
   #root_path
