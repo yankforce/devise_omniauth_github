c0ae1e8865393115f8247d54b76e992638b05f3f
Ken Lu <ken@seeice.com>
2013-06-23 15:46:31 -0400

    omniauth configuration for devise changed according to basic

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :developer unless Rails.env.production?
-#TODO put me to config app
-  provider :github, '6d7b4f715d4b81baedd3', 'c01e5c3688080c31cbc0205b87ca48eb98ae1d85'#ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  OmniAuth.config.logger = Rails.logger
-end
